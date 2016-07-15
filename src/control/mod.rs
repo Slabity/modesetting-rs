784cc486797ca86676bf9de4549dd3a01d80807a
Tyler Slabinski <tslabinski@slabity.net>
2016-07-15 16:20:46 -0400

    Added resource iterator types

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -1,15 +1,15 @@
 use super::ffi;
 use super::error::{Error, Result};
 
-use libc::ioctl;
-
-use std::os::unix::io::{RawFd, AsRawFd, FromRawFd, IntoRawFd};
+use std::os::unix::io::{RawFd, AsRawFd, FromRawFd};
 use std::fs::{File, OpenOptions};
 use std::path::Path;
 use std::sync::Arc;
 use std::mem::transmute;
+use std::iter::Iterator;
+use std::vec::IntoIter;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Device {
     file: Arc<File>
 }
@@ -41,7 +41,7 @@ impl Device {
 
         unsafe {
             Ok(Resources {
-                device: self.file.clone(),
+                device: (*self).clone(),
                 connectors: transmute(res.connectors),
                 encoders: transmute(res.encoders),
                 crtcs: transmute(res.crtcs),
@@ -51,21 +51,99 @@ impl Device {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ConnectorId(u32);
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct EncoderId(u32);
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct CrtcId(u32);
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FramebufferId(u32);
+
+pub struct ConnectorIterator {
+    device: Device,
+    connectors: IntoIter<ConnectorId>
+}
+
+pub struct EncoderIterator {
+    device: Device,
+    encoders: IntoIter<EncoderId>
+}
+
+pub struct CrtcIterator {
+    device: Device,
+    crtcs: IntoIter<CrtcId>
+}
+
+pub struct FramebufferIterator {
+    device: Device,
+    framebuffers: IntoIter<FramebufferId>
+}
+
+impl Iterator for ConnectorIterator {
+    type Item = ConnectorId;
+    fn next(&mut self) -> Option<ConnectorId> {
+        self.connectors.next()
+    }
+}
+
+impl Iterator for EncoderIterator {
+    type Item = EncoderId;
+    fn next(&mut self) -> Option<EncoderId> {
+        self.encoders.next()
+    }
+}
+
+impl Iterator for CrtcIterator {
+    type Item = CrtcId;
+    fn next(&mut self) -> Option<CrtcId> {
+        self.crtcs.next()
+    }
+}
+
+impl Iterator for FramebufferIterator {
+    type Item = FramebufferId;
+    fn next(&mut self) -> Option<FramebufferId> {
+        self.framebuffers.next()
+    }
+}
 
 #[derive(Debug)]
 pub struct Resources {
-    device: Arc<File>,
+    device: Device,
     connectors: Vec<ConnectorId>,
     encoders: Vec<EncoderId>,
     crtcs: Vec<CrtcId>,
     framebuffers: Vec<FramebufferId>
+}
+
+impl Resources {
+    pub fn connectors(&self) -> ConnectorIterator {
+        ConnectorIterator {
+            device: self.device.clone(),
+            connectors: self.connectors.clone().into_iter()
+        }
+    }
+
+    pub fn encoders(&self) -> EncoderIterator {
+        EncoderIterator {
+            device: self.device.clone(),
+            encoders: self.encoders.clone().into_iter()
+        }
+    }
+
+    pub fn crtcs(&self) -> CrtcIterator {
+        CrtcIterator {
+            device: self.device.clone(),
+            crtcs: self.crtcs.clone().into_iter()
+        }
+    }
+
+    pub fn framebuffers(&self) -> FramebufferIterator {
+        FramebufferIterator {
+            device: self.device.clone(),
+            framebuffers: self.framebuffers.clone().into_iter()
+        }
+    }
 }
 
