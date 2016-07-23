331631d62e1381539d6b172b3dcbb0cbf3e9e0a8
Tyler Slabinski <tslabinski@slabity.net>
2016-07-22 22:41:04 -0400

    Added NotAvailable error result

--- src/error.rs
+++ src/error.rs
@@ -4,12 +4,14 @@ use std::fmt;
 use std::error::Error as StdError;
 use std::result::Result as StdResult;
 use std::io::Error as IoError;
+use std::io::Write;
 use std::convert::From;
 
 #[derive(Debug)]
 pub enum Error {
     Io(IoError),
-    Ioctl(Errno)
+    Ioctl(Errno),
+    NotAvailable
 }
 
 pub type Result<T> = StdResult<T, Error>;
@@ -18,7 +20,8 @@ impl fmt::Display for Error {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::Io(ref err) => err.fmt(fmt),
-            Error::Ioctl(ref err) => err.fmt(fmt)
+            Error::Ioctl(ref err) => err.fmt(fmt),
+            _ => write!(fmt, "Not available")
         }
     }
 }
